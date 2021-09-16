cf0e9a5fac2ba7c732956d1281fd2f8c896eb335
MrFranQx <63866825+MrFranQx@users.noreply.github.com>
2021-09-16 23:12:24 +0200

    Somewhat improved readability and added some comments

--- contract.h
+++ contract.h
@@ -1,3 +1,21 @@
+/*
+	Contract - a small contract support header
+	Author: MrFranQx
+
+	How to use:
+	1. #include this header
+	2. Optionally set default_action
+	3. Use expects() at the beginning of the function to check its arguments
+	   and/or ensures() at the end of the function to check the return value
+
+	How it works:
+	It either throws an exception or terminates the program, based on default_action.
+	expects() and ensures() is the same except of the message;
+	they have to be separate functions to express they intent more clearly.
+
+	More info in README.md
+*/
+
 #pragma once
 
 #ifndef CONTRACT_H
@@ -11,9 +29,12 @@ namespace Contract {
 		exception = 0,
 		terminate = 1
 	};
+	// exception thrown when contract is violated
 	class Contract_violation : public std::logic_error { using std::logic_error::logic_error; };
+	
 	Action default_action = Action::exception;
-	void expects(bool expr)
+
+	void expects(bool expr) // expresses argument expectations
 	{
 		if (!expr) {
 			if (default_action == Action::exception)
@@ -24,7 +45,8 @@ namespace Contract {
 				throw std::logic_error("Contract check handle failed");
 		}
 	}
-	void ensures(bool expr)
+	
+	void ensures(bool expr) // expresses return value assurance
 	{
 		if (!expr) {
 			if (default_action == Action::exception)
